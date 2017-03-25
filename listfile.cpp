28598c8861ae31902d568bb3658ec85c2be23076
sarateraitetia <sarateraitetia@hotmail.fr>
2017-03-25 20:26:13 +0100

    add listfile .h .cpp

--- listfile.cpp
+++ listfile.cpp
@@ -1,1 +1,15 @@
+#include "listfile.h"
 
+// Constructor(s)
+ListFile::ListFile(){
+
+}
+
+// Method(s)
+ListFile::getWord(int indice) {
+
+}
+
+ListFile::searchWord(string word) {
+
+}
--- listfile.h
+++ listfile.h
@@ -1,5 +1,31 @@
 #ifndef LISTFILE
 #define LISTFILE
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+class ListFile {
+
+private:
+    // Attribut(s)
+    int nbr;
+    vector<string> ListWords;
+
+public:
+    // Constructor(s)
+    ListFile();
+
+    // Method(s)
+
+    /// Return the word searching in the current list
+    string getWord(int indice);
+
+    /// Search the word in the list
+    /// If this word is in the list, return his indice
+    /// Else return -1
+    int searchWord(string word);
+};
 
 #endif // LISTFILE
 
