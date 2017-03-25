ea2145163f73e36740d039da0919afddbdb9201a
william00007 <byakuraii172@gmail.com>
2017-03-25 20:22:25 +0100

    ptite bidouille en attendant sara

--- main.cpp
+++ main.cpp
@@ -8,5 +8,13 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.show();
     Loader myLoader((char*)"myDataBase.txt");
+
+    std::vector<string> m_listEnglish;
+    m_listEnglish.push_back("ok");
+    m_listEnglish.push_back("pas bon");
+    m_listEnglish.push_back("voici");
+
+    int v1 = rand() % m_listEnglish.size();
+
     return a.exec();
 }
